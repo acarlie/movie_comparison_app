a31615708039ea200fc58bfda3fe977b2b2bde38
acarlie <ameliacarlie@gmail.com>
2019-08-10 13:53:01 -0400

    Added functionality to close buttons, created moviesArray

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,23 @@
 var app = {
     compare: false,
+    moviesArray:[],
     addMovie(el){
         event.preventDefault();
         var movie = el.val();
+        this.moviesArray.push(movie);
         app.generateButtons(movie);
         el.val('');
+    },
+    deleteAddedMovie(){
+        event.preventDefault();
+        $(this).parent().remove();
+        var dataId = $(this).parent().attr('data-id');
+        app.moviesArray = app.arrayRemove(app.moviesArray, dataId);
+    },
+    arrayRemove(arr, value) {
+        return arr.filter(function(ele){
+            return ele != value;
+        });
     },
     generateButtons(movie){
         var wrap = $('<div>').addClass('movie-wrap').attr('data-id', movie);
@@ -16,9 +29,27 @@ var app = {
     compare(){
         event.preventDefault();
         //generate comparison page
+    },
+    getOMDB(movie){ //so we can reuse this function using app.getOMDB(movie);
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            var ratingName = response.Ratings[0].Source;
+            var ratingValue = response.Ratings[0].Value;
+            console.log(response)
+            console.log(ratingName);
+            console.log(ratingValue);
+            
+        });
     }
 }
 
+$(document).on('click', '.button-delete', app.deleteAddedMovie);
+
+$('#compareMovies').on('click', app.compare);
 $('#addMovie').keypress(function(e){
     var key = e.which;
     var el = $(this);
@@ -27,15 +58,8 @@ $('#addMovie').keypress(function(e){
     } 
 });
 
-$(document).on('click', '.button-delete', function(){
-    event.preventDefault();
-    $(this).parent().remove();
-})
-
-$('#compareMovies').on('click', app.compare);
-
 // needs more work once HTML is finished
-var movie = "trolls" 
+var movie = "Die Hard"; 
 
 var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
